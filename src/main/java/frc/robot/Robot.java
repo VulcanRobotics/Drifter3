072990ab8a63f8b5e80ac105c2249d3bf82f4d24
PRandall123 <PRandall@SCH.org>
2018-11-28 00:09:15 -0500

    Added Drift code to Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,4 +1,4 @@
-/*----------------------------------------------------------------------------*/
+//PGR 5:13
 /* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
@@ -8,8 +8,27 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+//import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
+//import com.kauailabs.navx.frc.AHRS.SerialDataType;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.DriverStation;
+
+import edu.wpi.first.wpilibj.Joystick;
+//import edu.wpi.first.wpilibj.PIDController;
+//import edu.wpi.first.wpilibj.PIDOutput;
+//import edu.wpi.first.wpilibj.SPI;
+
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+//import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -19,10 +38,51 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends IterativeRobot {
+  AHRS ahrs;
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+/*  Initialize PID controller for Turn to degrees
+	PIDController turnController;
+	double rotateToAngleRate;
+	// The following PID Controller coefficients will need to be tuned 
+    // to match the dynamics of your drive system.  Note that the      
+    // SmartDashboard in Test mode has support for helping you tune    
+    // controllers by displaying a form where you can enter new P, I,  
+    // and D constants and test the mechanism.                         
+    
+    static final double kP = 0.03;
+    static final double kI = 0.00;
+    static final double kD = 0.00;
+    static final double kF = 0.00;
+    
+    static final double kToleranceDegrees = 2.0f;
+*/
+static final double kP = 0.03;
+
+//private static final String kDefaultAuto = "Default";
+//private static final String kCustomAuto = "My Auto";
+//private String m_autoSelected;
+//private SendableChooser<String> m_chooser = new SendableChooser<>();
+private WPI_TalonSRX LeftDrive1;
+private WPI_TalonSRX LeftDrive2;
+private WPI_TalonSRX LeftDrive3;
+private WPI_TalonSRX LeftDrive4;
+private WPI_TalonSRX RightDrive1;
+private WPI_TalonSRX RightDrive2;
+private WPI_TalonSRX RightDrive3;
+private WPI_TalonSRX RightDrive4;
+private Joystick DriverJoystick;
+private DifferentialDrive DriveBase;	
+private int MaxAmps = 40;
+private int MaxAmpsTimeOut = 1000;
+//private boolean isInverted = true;
+
+
+
+
 
   /**
    * This function is run when the robot is first started up and should be
@@ -33,6 +93,84 @@ public class Robot extends IterativeRobot {
     m_chooser.addDefault("Default Auto", kDefaultAuto);
     m_chooser.addObject("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+
+// Define Left drive with Even CAN IDs
+LeftDrive1 = new WPI_TalonSRX(52);
+LeftDrive2 = new WPI_TalonSRX(54);
+LeftDrive3 = new WPI_TalonSRX(56);
+LeftDrive4 = new WPI_TalonSRX(58);
+
+//Invert motor direction
+//LeftDrive1.setInverted(isInverted);
+//LeftDrive2.setInverted(isInverted);
+//LeftDrive3.setInverted(isInverted);
+//LeftDrive4.setInverted(isInverted);
+
+// Define right drive with Odd CAN IDs
+RightDrive1 = new WPI_TalonSRX(51);
+RightDrive2 = new WPI_TalonSRX(53);
+RightDrive3 = new WPI_TalonSRX(55);
+RightDrive4 = new WPI_TalonSRX(57);
+
+//Invert motor direction
+//RightDrive1.setInverted(isInverted);
+//RightDrive2.setInverted(isInverted);
+//RightDrive3.setInverted(isInverted);
+//RightDrive4.setInverted(isInverted);
+
+DriverJoystick = new Joystick(0);
+
+// Set up 3 motor controllers to follow master
+LeftDrive2.set(ControlMode.Follower, LeftDrive1.getDeviceID());
+LeftDrive3.set(ControlMode.Follower, LeftDrive1.getDeviceID());
+LeftDrive4.set(ControlMode.Follower, LeftDrive1.getDeviceID());
+
+RightDrive2.set(ControlMode.Follower, RightDrive1.getDeviceID());
+RightDrive3.set(ControlMode.Follower, RightDrive1.getDeviceID());
+RightDrive4.set(ControlMode.Follower, RightDrive1.getDeviceID());
+
+// Link motors into arcade or Differntial Drive
+DriveBase = new DifferentialDrive(LeftDrive1, RightDrive1);
+//DriveBase = new CurvatureDrive(LeftDrive1, RightDrive1);
+
+// Set current limits on motors on Master, Slaves should follow
+LeftDrive1.configContinuousCurrentLimit(MaxAmps,MaxAmpsTimeOut);
+RightDrive1.configContinuousCurrentLimit(MaxAmps,MaxAmpsTimeOut);
+
+
+//set up camera server
+CameraServer.getInstance().startAutomaticCapture();
+
+  try {
+    /***********************************************************************
+     * navX-MXP:
+     * - Communication via RoboRIO MXP (SPI, I2C, TTL UART) and USB.            
+     * - See http://navx-mxp.kauailabs.com/guidance/selecting-an-interface.
+     * 
+     * navX-Micro:
+     * - Communication via I2C (RoboRIO MXP or Onboard) and USB.
+     * - See http://navx-micro.kauailabs.com/guidance/selecting-an-interface.
+     * 
+     * Multiple navX-model devices on a single robot are supported.
+     ************************************************************************/
+          ahrs = new AHRS(SerialPort.Port.kUSB1);
+          //ahrs = new AHRS(SerialPort.Port.kMXP, SerialDataType.kProcessedData, (byte)50);
+          ahrs.enableLogging(true);
+      } catch (RuntimeException ex ) {
+          DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
+      }
+      Timer.delay(1.0);
+      
+      
+      LeftDrive1.configSelectedFeedbackSensor(com.ctre.phoenix.motorcontrol.FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+      RightDrive1.configSelectedFeedbackSensor(com.ctre.phoenix.motorcontrol.FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+      
+      /* Setup sensors to check status, can also be used for phasing */
+      RightDrive1.configSelectedFeedbackSensor(com.ctre.phoenix.motorcontrol.FeedbackDevice.QuadEncoder, 0, 0);
+      RightDrive1.setSensorPhase(false);
+      LeftDrive1.configSelectedFeedbackSensor(com.ctre.phoenix.motorcontrol.FeedbackDevice.QuadEncoder, 0, 0);
+      LeftDrive1.setSensorPhase(false);
+
   }
 
   /**
@@ -87,6 +225,32 @@ public class Robot extends IterativeRobot {
    */
   @Override
   public void teleopPeriodic() {
+
+	// Configure Arcade drive note Joystick Y direction is reversed
+	DriveBase.curvatureDrive(-DriverJoystick.getY(),DriverJoystick.getX(),DriverJoystick.getRawButton(1));
+	SmartDashboard.putNumber("Velocity: ", -DriverJoystick.getY());
+	SmartDashboard.putNumber("Turning:  ", DriverJoystick.getX());
+	SmartDashboard.putBoolean("Quick:  ", DriverJoystick.getTrigger());
+	
+	// Note you must enable teleop to populate new widgets
+    SmartDashboard.putNumber(   "IMU_Yaw",              ahrs.getYaw());
+    SmartDashboard.putNumber(   "IMU_Pitch",            ahrs.getPitch());
+    SmartDashboard.putNumber(   "IMU_Roll",             ahrs.getRoll());
+
+    // use thumb button to reset gyro yaw
+    if ( DriverJoystick.getTop()) 
+    	{
+        	ahrs.zeroYaw();
+    	}
+    
+
+    /* Output value to SmartDashboard */
+    SmartDashboard.putNumber("Right Sensor position", RightDrive1.getSelectedSensorVelocity(0));
+    SmartDashboard.putNumber("Left Sensor Velocity", LeftDrive1.getSelectedSensorVelocity(0));
+ 
+    
+
+
   }
 
   /**

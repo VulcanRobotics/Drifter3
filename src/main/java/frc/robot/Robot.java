f54699c86a7ce59810ad45503d2f9b5f14484ead
Simon Kioko <skioko@skioko-2019.sch.org>
2018-12-03 08:34:51 -0500

    SMK Changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,4 +1,5 @@
 //PGR 5:13
+//Hi My name is Simon and I was here too
 /* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
